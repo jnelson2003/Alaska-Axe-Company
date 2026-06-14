16a5a2ebc15c713c076bda28e9666237c6bc0456
Jackson <101430418+jnelson2003@users.noreply.github.com>
2026-06-13 19:43:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 window.addEventListener('load', function() {
     const loader = document.getElementById('loader');
     
-    // 300ms delay to see the spin effect
-    setTimeout(function() {
-        loader.classList.add('loader-hidden');
-    },300); 
+    // Only run the hiding logic if the loader actually exists on this page
+    if (loader) {
+        // 300ms delay to see the spin effect
+        setTimeout(function() {
+            loader.classList.add('loader-hidden');
+        }, 300); 
+    }
 });
