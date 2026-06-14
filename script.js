2bf54d80140c06a3f105ca139a7f8fb1a99b68d2
Jackson <101430418+jnelson2003@users.noreply.github.com>
2026-06-13 20:15:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ window.addEventListener('load', function() {
         }, 300); 
     }
 });
+
+// Testimonials Interactive Logic
+document.addEventListener('DOMContentLoaded', function() {
+    const tCards = document.querySelectorAll(".testimonial-card");
+    const tContentBoxes = document.querySelectorAll(".testimonial-contentBox");
+
+    if (tCards.length > 0) {
+        for (let i = 0; i < tCards.length; i++) {
+            tCards[i].addEventListener("mouseover", function () {
+                // Reset all content boxes
+                for (let j = 0; j < tContentBoxes.length; j++) {
+                    tContentBoxes[j].className = "testimonial-contentBox";
+                }
+                
+                // Show matched content box
+                document.getElementById(this.dataset.id).className = "testimonial-contentBox active";
+
+                // Reset all cards
+                for (let k = 0; k < tCards.length; k++) {
+                    tCards[k].className = "testimonial-card";
+                }
+                
+                // Highlight active card
+                this.className = "testimonial-card active";
+            });
+        }
+    }
+});
